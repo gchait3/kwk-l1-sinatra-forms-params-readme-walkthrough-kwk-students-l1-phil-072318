0afbdd361595b9214561d4807a933c0d22c24d9f
Grace T <gtsai64@gmail.com>
2018-08-02 14:48:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/model.rb'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
+  get '/' do
     erb :food_form
+  end
+  
+  post '/' do
+    @my_result = get_spooked(params[:question1])
+    erb :results
   end
 
   # Add your post route and action below
